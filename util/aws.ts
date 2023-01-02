5b6a6bb3a9fb2ce2d220780dff9cebfb61b97d22
Kalka <k@kalka.io>
2023-01-02 08:31:10 -0500

    update environment variables

--- util/aws.ts
+++ util/aws.ts
@@ -5,8 +5,8 @@ import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter"
 const config: DynamoDBClientConfig = {
   region: "us-east-1",
   credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+      accessKeyId: process.env.APP_AWS_ACCESS_KEY_ID as string,
+      secretAccessKey: process.env.APP_AWS_SECRET_ACCESS_KEY as string,
   },
 }
 
